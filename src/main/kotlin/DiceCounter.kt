61a0b75e7488413a97e5ebb10f6539731185f12b
Eldar Singin <eldar.singin@glovoapp.com>
2023-05-12 17:27:11 +0300

    Implemented example 2

--- src/main/kotlin/DiceCounter.kt
+++ src/main/kotlin/DiceCounter.kt
@@ -2,7 +2,7 @@ class DiceCounter {
   fun howManyPossibilitiesToObtain(numberOfDice: Int, targetNumber: Int): Int =
     when(numberOfDice) {
       1 -> if (targetNumber in 1..6) 1 else 0
-
+      2 -> if (targetNumber in 2..7) targetNumber - 1 else 13 - targetNumber
       else -> TODO()
     }
 }
--- src/test/kotlin/DiceCounterTest.kt
+++ src/test/kotlin/DiceCounterTest.kt
@@ -14,7 +14,6 @@ class DiceCounterTest {
         assertEquals(expectedCombinations, result)
     }
 
-    @Disabled("Do ex1 first")
     @ParameterizedTest
     @CsvSource("2, 1", "3, 2", "4, 3", "5, 4", "6, 5", "7, 6", "8, 5", "9, 4", "10, 3", "11, 2", "12, 1", "13, 0")
     fun `EX2_given two dices we get the expected result`(targetNumber: Int, expectedCombination: Int) {
