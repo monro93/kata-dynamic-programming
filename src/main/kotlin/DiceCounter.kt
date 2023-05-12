eb827dfc4948a5c828f4cceacbfc1b168d8ec1af
Angelo Sciarra <angelo.sciarra@glovoapp.com>
2023-05-12 16:12:12 +0200

    added implementation of test for 1 dice

--- src/main/kotlin/DiceCounter.kt
+++ src/main/kotlin/DiceCounter.kt
@@ -1,5 +1,8 @@
 class DiceCounter {
-    fun howManyPossibilitiesToObtain(numberOfDice: Int, sumOfDices: Int): Int {
-        TODO()
+  fun howManyPossibilitiesToObtain(numberOfDice: Int, targetNumber: Int): Int =
+    when(numberOfDice) {
+      1 -> if (targetNumber in 1..6) 1 else 0
+
+      else -> TODO()
     }
 }
--- src/test/kotlin/DiceCounterTest.kt
+++ src/test/kotlin/DiceCounterTest.kt
@@ -8,28 +8,28 @@ class DiceCounterTest {
 
     @ParameterizedTest
     @CsvSource("1, 1", "2, 1", "3, 1", "4, 1", "5, 1", "6, 1", "7, 0", "-1, 0")
-    fun `Ex1_given one dice we get the expected result`(sumOfDices: Int, expected: Int) {
+    fun `Ex1_given one dice we get the expected result`(targetNumber: Int, expectedCombinations: Int) {
         val diceCounter = DiceCounter()
-        val result = diceCounter.howManyPossibilitiesToObtain(1, sumOfDices)
-        assertEquals(expected, result)
+        val result = diceCounter.howManyPossibilitiesToObtain(1, targetNumber)
+        assertEquals(expectedCombinations, result)
     }
 
     @Disabled("Do ex1 first")
     @ParameterizedTest
     @CsvSource("2, 1", "3, 2", "4, 3", "5, 4", "6, 5", "7, 6", "8, 5", "9, 4", "10, 3", "11, 2", "12, 1", "13, 0")
-    fun `EX2_given two dices we get the expected result`(sumOfDices: Int, expected: Int) {
+    fun `EX2_given two dices we get the expected result`(targetNumber: Int, expectedCombination: Int) {
         val diceCounter = DiceCounter()
-        val result = diceCounter.howManyPossibilitiesToObtain(2, sumOfDices)
-        assertEquals(expected, result)
+        val result = diceCounter.howManyPossibilitiesToObtain(2, targetNumber)
+        assertEquals(expectedCombination, result)
     }
 
     @Disabled("Do ex2 first")
     @ParameterizedTest
     @CsvSource("3, 15, 10", "4, 8, 35", "5, 12, 305", "7, 38, 210")
-    fun `EX3_given n dices we get the expected result`(numberOfDices: Int, sumOfDices: Int, expected: Int) {
+    fun `EX3_given n dices we get the expected result`(numberOfDices: Int, targetNumber: Int, expectedCombination: Int) {
         val diceCounter = DiceCounter()
-        val result = diceCounter.howManyPossibilitiesToObtain(numberOfDices, sumOfDices)
-        assertEquals(expected, result)
+        val result = diceCounter.howManyPossibilitiesToObtain(numberOfDices, targetNumber)
+        assertEquals(expectedCombination, result)
     }
 
     @Disabled("Do ex3 first")
